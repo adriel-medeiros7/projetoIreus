3589d6c5c5b8db5dbf5b0de126cf240f386577c0
Andrei <andreimaurinagcb@hotmail.com>
2020-10-18 17:08:02 -0300

    gravando hardware

--- src/app/hardware/hardware.page.ts
+++ src/app/hardware/hardware.page.ts
@@ -9,8 +9,43 @@ export class HardwarePage implements OnInit {
   constructor(){}
   ngOnInit() {}
 
-  gravarHardware(){
+  hardware: Hardware;
 
+  ionViewDidEnter(){
+    this.hardware = new Hardware();
+  }
+
+  listarHardware():Hardware[]{
+    let hardwares = [];
+    if (localStorage.getItem('hardware')) {
+      hardwares = JSON.parse(localStorage.getItem('hardware'));
+    }
+    return hardwares;
+  }
+
+  gravarHardware(){
+    let hardwares = this.listarHardware();
+    this.hardware.cod = Date.now()/100000;
+    hardwares.push(this.hardware);
+    localStorage.setItem('hardware', JSON.stringify(hardwares));
+    return true;
+  }
+
+  removerHardware():boolean{
+    let hardwares = this.listarHardware();
+    hardwares = hardwares.filter(
+      elemento => {
+        return elemento.cod != this.hardware.cod;
+      }
+    )
+    localStorage.setItem('hardware',JSON.stringify(hardwares));
+    return false;
   }
   
+}
+
+export class Hardware {
+  cod:any;
+  ident:any;
+  equip:number;
 }
