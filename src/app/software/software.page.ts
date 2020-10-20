3f12454b799df507258e7aa1706dc091d09564ac
Adriel <adriel.medeiros7@gmail.com>
2020-10-20 09:05:52 -0300

    mensagem dizendo oq vc fez

--- src/app/software/software.page.ts
+++ src/app/software/software.page.ts
@@ -12,5 +12,38 @@ export class SoftwarePage implements OnInit {
   ngOnInit() {
   }
 
-  // ID,NOME, setor, NIVEL, IDLICENCA, TEMLICENCA, CATEGORIA
+  software: Software;
+  empresas = JSON.parse(localStorage.getItem('empresa'));
+  
+  ionViewDidEnter(){
+    this.software = new Software();
+  }
+  
+  listarSoftware():Software[]{
+    let Softwares = [];
+    if (localStorage.getItem('software')) {
+      Softwares = JSON.parse(localStorage.getItem('software'));
+    }
+    return Softwares;
+  }
+  
+  gravarSoftware(){
+    let softwares = this.listarSoftware();
+    this.software.cod = Date.now()/100000;
+    softwares.push(this.software);
+    localStorage.setItem('software', JSON.stringify(softwares));
+    this.software = new Software();
+    return true;
+  }
+    
 }
+
+export class Software {
+  cod:any;
+  ident:any;
+  equip:number;
+  setor:String;
+  empresa:String
+}
+  
+
